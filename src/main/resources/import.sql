986d94b25da7cba330621f7a23019fb93e8e46a3
Jessica Coburn-Wade <coburn.jessica@btinternet.com>
2018-08-28 17:11:05 +0100

    forgot to create a branch so directly comitting to master

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Accounts (name, surename, accountID) VALUES ('John', 'Doe', 1234);
+INSERT INTO Accounts (name, surename, accountID) VALUES ('Jane', 'Doe', 1235);
+INSERT INTO Accounts (name, surename, accountID) VALUES ('Jim', 'Taylor', 1236);
